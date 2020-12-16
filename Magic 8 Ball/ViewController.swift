1701895fbfdc9694a3e48c7b471131e60173b7a9
Jaime <josba93@gmail.com>
2020-12-16 00:32:26 -0600

    Refactored code.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -20,14 +20,13 @@ class ViewController : UIViewController {
     //It will be changed when a user clicks a button
     @IBOutlet weak var eightBallImageView: UIImageView!
     
-    
+    //Setting an array of different answers
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
 
     @IBAction func clickButtonPressed(_ sender: UIButton) {
         
-        //Setting an array of different answers
-        let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
-        
-        //Once button is clicked we will aassing a random answer to display for user.
+        //NOTE the image is empty, a random image will display everytime
+        //after the user clicks the button
         eightBallImageView.image = ballArray.randomElement()
     }
 }
