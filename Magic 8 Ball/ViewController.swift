fcc055ed3242799035275c898df2a3919af16dac
Jaime <josba93@gmail.com>
2020-12-16 00:25:44 -0600

    Done creating 8ball app

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController : UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    
+    /*
+     //Define outlet for label
+     */
+    @IBOutlet weak var amaLabel: UILabel!
+    //Definine Image view for eightball
+    //It will be changed when a user clicks a button
+    @IBOutlet weak var eightBallImageView: UIImageView!
+    
+    
 
-
-
+    @IBAction func clickButtonPressed(_ sender: UIButton) {
+        
+        //Setting an array of different answers
+        let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
+        
+        //Once button is clicked we will aassing a random answer to display for user.
+        eightBallImageView.image = ballArray.randomElement()
+    }
 }
 
